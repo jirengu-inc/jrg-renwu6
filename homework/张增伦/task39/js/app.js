63988673416f318d1d883453da9b8d30c49a79fe
杭杭 <hanghang@hanghangdeMacBook-Pro.local>
2016-11-07 17:39:01 +0800

    task39

--- homework/张增伦/task39/js/app.js
+++ homework/张增伦/task39/js/app.js
@@ -7,9 +7,16 @@ requirejs.config({
 });
 
 
-requirejs(['jquery','person'],function   ($,Person) {
+requirejs(['jquery','person','carousel','gotop','exposure','waterfall'],function   ($,Person,Carousel,GoTop,Exposure,Waterfall) {
 
     var p = new Person('skylun');
     p.sayName();
+
+    new Carousel();
+    new GoTop($('body'),$('<div id="go-top">回到顶部</div>'));
+    new Exposure($(".timeline li"));
+    var waterfall = new Waterfall();
+	//waterfall.init();
+
 
 });
