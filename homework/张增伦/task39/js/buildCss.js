06798c93730c869ff92fbab78aa9ec53523bac77
杭杭 <hanghang@hanghangdeMacBook-Pro.local>
2016-11-07 13:26:56 +0800

    task39

---
+++ homework/张增伦/task39/js/buildCss.js
@@ -0,0 +1,19 @@
+/*
+({
+  baseUrl:'./app',
+  paths:{
+    lib: '../lib',
+    jquery: '../lib/jquery-1.11.3.min'
+  },
+  name:'../app',
+  out:"./dist/index.merge.min.js"
+})
+*/
+
+({
+    out : './dist/all.css',
+    cssIn: '../css/main.css',
+    optimizeCss:'standard'
+})
+
+
